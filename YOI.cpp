95f9a53228c81d10ddfaf889097611d4638eb9a7
Sarah Azka Arief <sarah_azka@yahoo.com>
2021-03-08 01:37:25 +0700

    Tugas 2 URO saya. LOL :V

---
+++ YOI.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+float func(float x){
+	return (x+77)/7;
+}
+
+float kalku(float a, float b, char op){
+	float hasil = 0;
+	switch(op){
+		case('+'):
+			hasil = a + b;
+			break;
+		case('-'):
+			hasil = a - b;
+			break;
+		case('*'):
+			hasil = a * b;
+			break;
+		case('/'):
+			hasil = a/b;
+			break;
+		case('^'):
+			hasil = 1;
+			for(int i=0; i<b; i++){
+				hasil *= a;
+			}
+			break;
+		case('&'):
+			float panjang = (b-a)/100;
+			for(int i=0; i < 100; i++){
+				hasil += ((func(a+(i*panjang)) + func(a+(i*panjang)+panjang))*panjang/2);
+				}
+			break;
+		}
+	return hasil;	
+}
+
+int main (){
+	float a;
+	float b;
+	char op;
+	string mshmogk;
+	cout << "Hai! Yuk cobain kalku aku!";
+	cout << endl;
+	while(true){
+		cout << "Masukkan angka pertama: ";
+		cin >> a;
+		cout << endl;
+		cout << "Masukkan angka kedua: ";
+		cin >> b;
+		cout << endl;
+		cout << "Masukkan operan: ";
+		cin >> op;
+		cout << kalku(a, b, op) << endl;
+		cout << "Masih mo gk? :V (kalo mau tulis :V, kalo ga tulis :'V')" << endl;
+		cin >> mshmogk;
+		if (mshmogk == ":'V") break;
+	}
+}
+	
